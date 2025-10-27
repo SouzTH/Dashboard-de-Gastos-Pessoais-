086243ce61d8d27019486a4bbf861f72d0643fe8
Witch <g.andrade2431@outlook.com>
2025-10-27 16:53:20 -0300

    knex tables wrote

--- back/knexfile.js
+++ back/knexfile.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 module.exports = {
   client: "mysql",
   connection: {
+    host: process.env.SERVER_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    host: process.env.SERVER_HOST,
-    port: process.env.DB_PORT,
   },
   migrations: {
     tableName: "knex_migrations",
