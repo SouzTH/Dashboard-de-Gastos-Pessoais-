95af71a7c3c14bb9b7145e464256a8995e3919bb
semdoce <g.andrade2431@outlook.com>
2025-10-29 16:40:07 -0300

    register page w/o button confirmation

--- front/src/components/registerForm.jsx
+++ front/src/components/registerForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { NavLink, Link } from "react-router";
 
-function registerForm(){
+function RegisterForm(){
+    const [nome,setNome] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [password2, setPassword2] = useState("");
@@ -8,28 +10,34 @@ function registerForm(){
     return (
       <>
         <p className="w-24 aspect-square rounded-full bg-white text-center"></p>
-        <div className="flex flex-col">
-            <p className="text-sm">Email</p>
-            <input className="bg-white rounded p-2" type="text" value={email} placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)} />
+        <div className="w-9/12 space-y-2">
+            <div className="flex flex-col">
+                <p className="text-sm">Nome</p>
+                <input className="bg-white rounded p-2" type="text" value={nome} placeholder="Seu Nome" onChange={(e) => setNome(e.target.value)} />
+            </div>
+            
+            <div className="flex flex-col">
+                <p className="text-sm">Email</p>
+                <input className="bg-white rounded p-2" type="text" value={email} placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            
+            <div className="flex flex-col">
+                <p className="text-sm">Senha</p>
+                <input className="bg-white rounded p-2" type="text" value={password} placeholder="Sua senha" onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            
+            
+            <div className="flex flex-col">
+                <p className="text-sm">Confirme a senha</p>
+                <input className="bg-white rounded p-2" type="text" value={password2} placeholder="Sua senha novamente" onChange={(e) => setPassword2(e.target.value)} />
+            </div>
         </div>
-        
-        <div className="flex flex-col">
-            <p className="text-sm">Senha</p>
-            <input className="bg-white rounded p-2" type="text" value={password} placeholder="Sua senha" onChange={(e) => setPassword(e.target.value)}/>
-        </div>
-        
-        
-        <div className="flex flex-col">
-            <p className="text-sm">Senha</p>
-            <input className="bg-white rounded p-2" type="text" value={password2} placeholder="Sua Senha novamente" onChange={(e) => setPassword2(e.target.value)}/>
-        </div>
-
-        <button className="rounded bg-violet-900 p-2 text-white">Cadastre-se</button>
+        <button className="rounded bg-violet-900 p-2 text-white w-9/12">Cadastre-se</button>
         <div className="flex space-x-2">
             <p>Já tem uma conta?</p>
-            <p className="text-violet-600">Faça login!</p>
-        </div>
+            <NavLink to="/login" className="text-violet-600">Faça login!</NavLink>
+        </div>        
       </>
     )
 }
-export default registerForm
+export default RegisterForm
