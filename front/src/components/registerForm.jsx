78281b20e85eb375108473d2f7bbbea8a7e129a8
semdoce <g.andrade2431@outlook.com>
2025-10-29 14:43:21 -0300

    register page w/o button confirmation

--- front/src/components/registerForm.jsx
+++ front/src/components/registerForm.jsx
@@ -8,9 +8,22 @@ function registerForm(){
     return (
       <>
         <p className="w-24 aspect-square rounded-full bg-white text-center"></p>
-        <input className="bg-white rounded p-2" type="text" value={email} placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} />
-        <input className="bg-white rounded p-2" type="text" value={password} placeholder="Senha" onChange={(e) => setPassword(e.target.value)}/>
-        <input className="bg-white rounded p-2" type="text" value={password2} placeholder="Senha novamente" onChange={(e) => setPassword2(e.target.value)}/>
+        <div className="flex flex-col">
+            <p className="text-sm">Email</p>
+            <input className="bg-white rounded p-2" type="text" value={email} placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        
+        <div className="flex flex-col">
+            <p className="text-sm">Senha</p>
+            <input className="bg-white rounded p-2" type="text" value={password} placeholder="Sua senha" onChange={(e) => setPassword(e.target.value)}/>
+        </div>
+        
+        
+        <div className="flex flex-col">
+            <p className="text-sm">Senha</p>
+            <input className="bg-white rounded p-2" type="text" value={password2} placeholder="Sua Senha novamente" onChange={(e) => setPassword2(e.target.value)}/>
+        </div>
+
         <button className="rounded bg-violet-900 p-2 text-white">Cadastre-se</button>
         <div className="flex space-x-2">
             <p>Já tem uma conta?</p>
--- front/src/pages/Register.jsx
+++ front/src/pages/Register.jsx
@@ -6,7 +6,7 @@ function DashboardRegister() {
     <>
     <div className="flex flex-col h-screen w-screen justify-center items-center">
       <h1 className="font-bold text-2xl text-center">Tela de Cadastro</h1>
-        <div className="h-9/12 w-4/12 bg-violet-200 p-12 flex flex-col justify-around items-center space-y-4 rounded-2xl border-violet-400 shadow-2xl">
+        <div className="min-h-9/12 w-4/12 bg-violet-200 p-12 flex flex-col justify-around items-center space-y-4 rounded-2xl border-2 border-violet-300 shadow-2xl">
         <RegisterForm/>
       </div>
     </div>
