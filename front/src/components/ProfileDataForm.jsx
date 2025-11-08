8f36d794afb6cc8822c9ed3796272b1c41011641
Rafael Souza <rafael.barrionuevo.dev@gmail.com>
2025-11-08 15:09:47 -0300

    Adicionando useEffect em ProfileDataForm

--- front/src/components/ProfileDataForm.jsx
+++ front/src/components/ProfileDataForm.jsx
@@ -1,93 +1,105 @@
-import React, { useState } from "react";
-import "../style/ConfiguracoesPerfil.css"
+import React, { useState, useEffect } from "react";
+import "../style/ConfiguracoesPerfil.css";
 
 //user pra preencher e handle pra salvar eles
-export default function ProfileDataForm({ user, handleUpdate}){
-    const [formData, setFormData] = useState({
-        nome: user.nome || "",
-        email: user.email || "",
-        senha: ""
+export default function ProfileDataForm({ user, handleUpdate }) {
+  const [formData, setFormData] = useState({
+    nome: user.nome || "",
+    email: user.email || "",
+    senha: "",
+  });
+
+  useEffect(() => {
+    setFormData({
+      nome: user.nome || "",
+      email: user.email || "",
+      senha: "",
     });
+  }, [user]);
 
-    const handleChange = (e) => {//pelo o que entendi, atualiza quando digita, e quando pega name do input, e value do input
-        setFormData({
-            ...formData, //mantem os outros dados
-            [e.target.name]: e.target.value //atualiza o que foi digitado
-        });
-    };
+  const handleChange = (e) => {
+    //pelo o que entendi, atualiza quando digita, e quando pega name do input, e value do input
+    setFormData({
+      ...formData, //mantem os outros dados
+      [e.target.name]: e.target.value, //atualiza o que foi digitado
+    });
+  };
 
-    //quando clica em salvar
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        if(!formData.nome || !formData.email){
-            return alert("Nome e email são obrigatórios.");
-        }
+  //quando clica em salvar
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try{
-            await handleUpdate(formData);
-            alert("Perfil atualizado com sucesso!");
-        }catch(error){
-            console.error("Erro ao atualizar perfil:", error);
-            alert("Erro ao salvar perfil.");
-        }
-    };
+    if (!formData.nome || !formData.email) {
+      return alert("Nome e email são obrigatórios.");
+    }
 
-    return (
-        <div className="form-container-geral">
-            <h3 className="form-titulo">Editar Perfil</h3>
+    try {
+      await handleUpdate(formData);
+      alert("Perfil atualizado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao atualizar perfil:", error);
+      alert("Erro ao salvar perfil.");
+    }
+  };
 
-            <form onSubmit={handleSubmit} className="perfil-form">
-                
-                {/* campo nome */}
-                <div className="grupo-campo">
-                    <label className="rotulo" htmlFor="nome">Nome:</label>
-                    <input
-                        type='text'
-                        id="nome"
-                        name="nome"
-                        value={formData.nome}
-                        onChange={handleChange}
-                        className="campo-input"
-                        required
-                    />
-                </div> 
+  return (
+    <div className="form-container-geral">
+      <h3 className="form-titulo">Editar Perfil</h3>
 
-                {/* campo email */}
-                <div className="grupo-campo">
-                    <label className="rotulo" htmlFor="email">Email:</label>
-                    <input
-                        type='email'
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className="campo-input"
-                        required
-                    />
-                </div>
-                {/* campo senha */}
-                <div  className="grupo-campo">
-                    <label className="rotulo" htmlFor="senha">Senha:</label>
-                    <input
-                        type='password'
-                        id="senha"
-                        name="senha"
-                        value={formData.senha}
-                        onChange={handleChange}
-                        className="campo-input"
-                        placeholder=""
-                    />
-                </div>
-                {/* botao salvar */}
-                <div>
-                    <button
-                        type="submit"
-                        className="botao-salvar">
-                        Salvar Alterações
-                    </button>
-                </div>
-            </form>
+      <form onSubmit={handleSubmit} className="perfil-form">
+        {/* campo nome */}
+        <div className="grupo-campo">
+          <label className="rotulo" htmlFor="nome">
+            Nome:
+          </label>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            value={formData.nome}
+            onChange={handleChange}
+            className="campo-input"
+            required
+          />
         </div>
-    );
+
+        {/* campo email */}
+        <div className="grupo-campo">
+          <label className="rotulo" htmlFor="email">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="campo-input"
+            required
+          />
+        </div>
+        {/* campo senha */}
+        <div className="grupo-campo">
+          <label className="rotulo" htmlFor="senha">
+            Senha:
+          </label>
+          <input
+            type="password"
+            id="senha"
+            name="senha"
+            value={formData.senha}
+            onChange={handleChange}
+            className="campo-input"
+            placeholder=""
+          />
+        </div>
+        {/* botao salvar */}
+        <div>
+          <button type="submit" className="botao-salvar">
+            Salvar Alterações
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
