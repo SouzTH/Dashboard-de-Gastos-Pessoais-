b0d0c4399089eaf0a33f95837fba57ecbff1f369
Gustavo <g.andrade2431@outlook.com>
2025-11-17 13:16:12 -0300

    transaction page integration axios

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
+import { TransactionProvider } from "./context/TransactionContext";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <TransactionProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </TransactionProvider>
     </UserProvider>
   </React.StrictMode>
 );
